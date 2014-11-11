48ae8a2cd135aee90bdaf306743dbb5aee99c45c
Sapein <Chandler182@gmail.com>
2014-11-11 17:05:48 -0600

    Attack can be done with more commands
    I have expanded the commands of attack and fixed that I should have exit up and running soon

--- main.c
+++ main.c
@@ -60,8 +60,7 @@ int main(){
 				}
 			}
 			fgets(playerInput, 40, stdin);
-			/* if((strcmp(playerInput, "1\n") == 0 || strcmp(playerInput, "Attk\n") == 0 || strcmp(playerInput, "Attack\n") == 0 || strcmp(playerInput, "attack\n") || strcmp(playerInput, "atk") == 0 || strcmp(playerInput, "atk\n") == 0 || strcmp(playerInput, "Atk\n"))){ */ 
-			if(strcmp(playerInput, "attack\n") == 0){
+			if(strcmp(playerInput, "1\n") == 0 || strcmp(playerInput, "Attk\n") == 0 || strcmp(playerInput, "Attack\n") == 0 || strcmp(playerInput, "attack\n") == 0 || strcmp(playerInput, "atk\n") == 0 || strcmp(playerInput, "atk\n") == 0 || strcmp(playerInput, "Atk\n") == 0){ 
 				enemyHP = enemyDamage;
 				printf("You attacked the Enemy!\n");
 				if(enemyHP <= 0){
@@ -69,10 +68,6 @@ int main(){
 					printf("You Win! Summoning next Monster!\n"); 
 				}
 			}
-			/* else if(playerInput == ("2\n" || "Def\n" || "Defense\n" || "Defend\n" || "def\n" || "defense\n" || "defend\n")){ */
-			/* 	printf("You Defend!\n"); */
-			/* } */
-			/* else if((strcmp(playerInput, "2\n") == 0 || strcmp(playerInput, "exit\n") == 0 || strcmp(playerInput, "Exit\n") == 0 || strcmp(playerInput, "ext\n") == 0)){ */
 			else if(strcmp(playerInput, "exit\n") == 0){
 				battleOn = 0;
 				gameOn = 0; 
@@ -82,7 +77,6 @@ int main(){
 			}
 		}
 	}
-
 	return 0;
 }
 
